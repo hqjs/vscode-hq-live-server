0d2967a6f98a30556046619f151ff063f345065f
hqjs <hqjsorg@gmail.com>
2019-08-29 15:14:45 +0300

    Fix patch-sass

--- patch-sass.js
+++ patch-sass.js
@@ -1,8 +1,5 @@
 const fs = require('fs-extra');
 
-console.log('Removing node-sass');
-fs.removeSync('node_modules/node-sass');
-
 console.log('Patching postcss-node-sass');
 const postCSSNodeSass = fs.readFileSync('node_modules/postcss-node-sass/index.js', { encoding: 'utf8' });
 fs.writeFileSync(
